0df79b8c0d16cf4f1854f639e5006297b004c47d
AdrienPoupa <adrien@poupa.fr>
2018-02-09 18:17:19 -0500

    Increment the CBO count on redo #3

--- violet-framework/src/main/java/com/horstmann/violet/workspace/editorpart/behavior/UndoRedoOnAddBehavior.java
+++ violet-framework/src/main/java/com/horstmann/violet/workspace/editorpart/behavior/UndoRedoOnAddBehavior.java
@@ -209,6 +209,11 @@ public class UndoRedoOnAddBehavior extends AbstractEditorPartBehavior
                 {
                     super.redo();
                     IGraph graph = editorPart.getGraph();
+                    // Increment the CBO count on redo
+                    aSelectedEdge.getStartNode().incrementCboCount();
+                    aSelectedEdge.getEndNode().incrementCboCount();
+                    aSelectedEdge.getStartNode().updateCbo();
+                    aSelectedEdge.getEndNode().updateCbo();
                     graph.connect(aSelectedEdge, aSelectedEdge.getStartNode(), aSelectedEdge.getStartLocation(), aSelectedEdge.getEndNode(), aSelectedEdge.getEndLocation(), aSelectedEdge.getTransitionPoints());
                 }
             };
