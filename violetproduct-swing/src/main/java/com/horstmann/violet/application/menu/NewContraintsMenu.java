d46b737edc4da2bdb58911a66c796b5c19159e8e
Jehlum <k2@karakrum>
2018-02-12 15:39:45 -0500

    Displays No recursion when there is none.

--- violetproduct-swing/src/main/java/com/horstmann/violet/application/menu/NewContraintsMenu.java
+++ violetproduct-swing/src/main/java/com/horstmann/violet/application/menu/NewContraintsMenu.java
@@ -91,6 +91,8 @@ public class NewContraintsMenu extends JMenu {
 							dialogFactory.showErrorDialog("Recussion");
 						}
 					}
+					if(!rflag)
+						dialogFactory.showErrorDialog("No Recussion");
 				}
 			}
 		});
